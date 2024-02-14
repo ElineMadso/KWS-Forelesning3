c3da1067673dfd59400e73e16c6938a77729516e
Eline Madsø <elinemadso@gmail.com>
2024-02-14 17:52:12 +0100

    showing kommunenavn i overlay

--- src/modules/kommune/KommuneLayerCheckbox.tsx
+++ src/modules/kommune/KommuneLayerCheckbox.tsx
@@ -1,29 +1,37 @@
-import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
+import React, {
+  Dispatch,
+  MutableRefObject,
+  SetStateAction,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { Layer } from "ol/layer";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 import { GeoJSON } from "ol/format";
-import {Feature, Map, MapBrowserEvent} from "ol";
-import {Polygon} from "ol/geom";
+import { Feature, Map, MapBrowserEvent, Overlay } from "ol";
+import { Polygon } from "ol/geom";
 
 type KommuneProperties = {
-    kommunenummer: string;
-    navn: {
-        sprak: string,
-        navn: string,
-    }[];
+  kommunenummer: string;
+  navn: {
+    sprak: string;
+    navn: string;
+  }[];
 };
 
 type KommuneFeatures = Feature<Polygon> & {
-    getProperties(): KommuneProperties
+  getProperties(): KommuneProperties;
 };
 
 const kommuneSource = new VectorSource<KommuneFeatures>({
-    url: "/kommuner_komprimert.json",
-    format: new GeoJSON(),
+  url: "/kommuner_komprimert.json",
+  format: new GeoJSON(),
 });
 const kommunelayer = new VectorLayer({
-    source: kommuneSource,
+  source: kommuneSource,
 });
 
 export function KommuneLayerCheckbox({
@@ -34,13 +42,36 @@ export function KommuneLayerCheckbox({
   setLayers: Dispatch<SetStateAction<Layer[]>>;
 }) {
   const [checked, setChecked] = useState(false);
-  function handleClick(e: MapBrowserEvent<MouseEvent>) {
-    const clickedKommune = kommuneSource.getFeaturesAtCoordinate(e.coordinate,) as KommuneFeatures[];
-    if(clickedKommune.length === 1){
-        const properties = clickedKommune[0].getProperties() as KommuneProperties;
-        alert(properties.navn.find(n => n.sprak === "nor")!.navn);
-    }
 
+  const overlay = useMemo(() => new Overlay({}), []);
+
+  const overlayRef = useRef() as MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    overlay.setElement(overlayRef.current);
+    map.addOverlay(overlay);
+    return () => {
+      map.removeOverlay(overlay);
+    };
+  }, []);
+
+  //forteller hvilken kommune som er valgt
+  const [selectedKommune, setSelectedKommune] = useState<
+    KommuneFeatures | undefined
+  >();
+  function handleClick(e: MapBrowserEvent<MouseEvent>) {
+    const clickedKommune = kommuneSource.getFeaturesAtCoordinate(
+      e.coordinate,
+    ) as KommuneFeatures[];
+    if (clickedKommune.length === 1) {
+      setSelectedKommune(clickedKommune[0]);
+      overlay.setPosition(e.coordinate);
+    } else {
+      setSelectedKommune(undefined);
+      overlay.setPosition(undefined);
+    }
+    const properties = clickedKommune[0].getProperties() as KommuneProperties;
+    //alert(properties.navn.find((n) => n.sprak === "nor")!.navn);
   }
 
   useEffect(() => {
@@ -66,6 +97,18 @@ export function KommuneLayerCheckbox({
         />
         {checked ? "hide" : "show"} kommune Layer
       </label>
+
+      <div ref={overlayRef}>
+        {selectedKommune && (
+          <>
+            {
+                (selectedKommune.getProperties() as KommuneProperties)
+                .navn.find((n) => n.sprak === "nor",
+                )!.navn
+            }
+          </>
+        )}
+      </div>
     </div>
   );
 }

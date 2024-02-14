b2864e98be26109d97ed4fe8786e15c8ad569a5c
Eline Madsø <elinemadso@gmail.com>
2024-02-14 15:40:43 +0100

    display header and menu

---
+++ src/modules/application/application.tsx
@@ -0,0 +1,30 @@
+import React, {MutableRefObject, useEffect, useRef} from "react";
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import {OSM} from 'ol/source';
+import {useGeographic} from 'ol/proj';
+import "./application.css";
+import 'ol/ol.css';
+
+useGeographic();
+
+const map = new Map({
+    layers: [ new TileLayer({source: new OSM()})],
+    view: new View({center: [10,59], zoom: 10 })
+})
+export function Application() {
+    const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
+    useEffect(() => {
+        map.setTarget(mapRef.current);
+    }, []);
+    return <>
+        <header>
+            <h1>Kommune Kart</h1>
+        </header>
+        <nav>
+            <a href={"#"}>Focus on me</a>
+        </nav>
+        <div ref={mapRef}></div>
+    </>;
+}
+

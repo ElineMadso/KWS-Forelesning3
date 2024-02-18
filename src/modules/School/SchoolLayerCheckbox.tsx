703556853fedf29dd2b5ba6091f8d167092a7531
Eline Madsø <elinemadso@gmail.com>
2024-02-18 17:14:13 +0100

    move schoolfeature to top level

--- src/modules/School/SchoolLayerCheckbox.tsx
+++ src/modules/School/SchoolLayerCheckbox.tsx
@@ -4,21 +4,38 @@ import { useLayer } from "../kommune/KommuneLayerCheckbox";
 import VectorSource from "ol/source/Vector";
 import { GeoJSON } from "ol/format";
 import { Circle, Fill, Stroke, Style } from "ol/style";
+import { Feature } from "ol";
+import { Point } from "ol/geom";
+import { FeatureLike } from "ol/Feature";
 
 const schoolLayer = new VectorLayer({
   source: new VectorSource({
     url: "/schools.json",
     format: new GeoJSON(),
   }),
-  //endrer utseende på punktene
-  style: new Style({
-    image: new Circle({
-      stroke: new Stroke({ color: "white", width: 2 }),
-      fill: new Fill({ color: "green" }),
-      radius: 4,
-    }),
-  }),
+  //snarvei til styling funksjonen
+  style: schoolStyle,
 });
+
+interface SchoolProperties {
+  antall_elever: number;
+}
+
+//ts, expect these features to be pointfeatures
+type SchoolFeature = { getProperties(): SchoolProperties } & Feature<Point>;
+
+//endrer utseende på punktene
+function schoolStyle(f: FeatureLike) {
+  //our featurelike is a schoolfeature
+  const feature = f as SchoolFeature;
+  return new Style({
+    image: new Circle({
+      stroke: new Stroke({ color: "white", width: 3 }),
+      fill: new Fill({ color: "blue" }),
+      radius: 3 + feature.getProperties().antall_elever / 150,
+    }),
+  });
+}
 
 export function SchoolLayerCheckbox() {
   const [checked, setChecked] = useState(true);
